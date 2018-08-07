4b5116a764d36948255891f1fbc89e0677a2608f
Andrea Moretti <andrea.moretti@zalando.de>
2018-08-07 20:48:10 +0200

    feat: jpeg dataclient

---
+++ src/optimal_image/dataclients/jpeg.rs
@@ -0,0 +1,39 @@
+extern crate dssim;
+extern crate image;
+extern crate imgref;
+extern crate lodepng;
+extern crate rgb;
+
+use dataclients::{ImageDataResult, Loader};
+use image::*;
+use rgb::RGBA;
+use std::path::Path;
+
+pub struct Jpeg {
+    pub name: String,
+}
+
+impl Loader<ImageError> for Jpeg {
+    fn load<P: AsRef<Path>>(&self, path: P) -> ImageDataResult<ImageError> {
+        let image = image::open(path)?;
+        let (width, height) = image.dimensions();
+        let buffer = image
+            .to_rgba()
+            .pixels()
+            .map(|pixel| {
+                let [r, g, b, a] = pixel.to_rgba().data;
+                let rgba: RGBA<f32>;
+                // convert u8 (0/255) color values to (0-1) f32 ranges
+                rgba = RGBA {
+                    r: r as f32 / 255.0,
+                    g: g as f32 / 255.0,
+                    b: b as f32 / 255.0,
+                    a: a as f32 / 255.0,
+                };
+                rgba
+            })
+            .collect();
+
+        Ok(imgref::Img::new(buffer, width as usize, height as usize))
+    }
+}
--- src/optimal_image/dataclients/mod.rs
+++ src/optimal_image/dataclients/mod.rs
@@ -1,14 +1,18 @@
 extern crate dssim;
+extern crate rgb;
 use imgref::ImgVec;
+use rgb::*;
 use std::error::Error;
 use std::path::Path;
+mod jpeg;
 mod png;
 
-pub type ImageData = ImgVec<dssim::RGBAPLU>;
-pub type ImageDataResult<E = Error> = Result<ImageData, E>;
+pub type ImageData = ImgVec<RGBA<f32>>;
+pub type ImageDataResult<E> = Result<ImageData, E>;
 
 pub trait Loader<E: Error> {
     fn load<P: AsRef<Path>>(&self, path: P) -> ImageDataResult<E>;
 }
 
+pub use self::jpeg::Jpeg;
 pub use self::png::Png;
