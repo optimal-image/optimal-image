f9e60e88c20ceca0e0766d9035d739f01fff8f33
Andrea Moretti <andrea.moretti@zalando.de>
2018-08-21 09:41:56 +0200

    wip: in memory vips operations

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
 extern crate dssim;
 extern crate optimal_image;
 
+use optimal_image::compress::*;
 use optimal_image::dataclients::*;
 use optimal_image::search::*;
 use optimal_image::ImageFormat;
@@ -13,14 +14,8 @@ fn main() {
     let a = &rest_args[0];
     let b = &rest_args[1];
 
-    let png_client = Png {
-        name: String::from("png-dataclient"),
-    };
-
-    let jpg_client = Jpeg {
-        name: String::from("jpeg-dataclient"),
-    };
-
+    let png_client = Png::new();
+    let jpg_client = Jpeg::new();
     let mut context = dssim::new();
     // let image_a = png_client.load(Path::new(a)).unwrap();
     // let image_b = png_client.load(Path::new(b)).unwrap();
@@ -32,18 +27,31 @@ fn main() {
     let dssim_image_a = context.create_image(&image_a).unwrap();
     let dssim_image_b = context.create_image(&image_b).unwrap();
 
-    let mut search = Search::from_path(
-        Path::new(a),
-        SearchOptions {
-            threshold: 0.004,
-            quality_range: (70, 91),
-            formats: vec![ImageFormat::JPEG, ImageFormat::PNG],
-        },
-    ).unwrap();
-    search.run();
-    println!("{:?}", search.get_result());
+    // let mut search = Search::from_path(
+    //     Path::new(a),
+    //     SearchOptions {
+    //         threshold: 0.004,
+    //         quality_range: (70, 91),
+    //         formats: vec![ImageFormat::JPEG, ImageFormat::PNG],
+    //     },
+    // ).unwrap();
+    // search.run();
+    // println!("{:?}", search.get_result());
 
-    let (val, _) = context.compare(&dssim_image_a, dssim_image_b);
+    // let (val, _) = context.compare(&dssim_image_a, dssim_image_b);
 
-    println!("{:?}", val);
+    // println!("{:?}", val);
+
+    let img = VipsImage::from_file(Path::new(a).to_str().unwrap()).unwrap();
+    let img2 = VipsImage::from_image_data(&image_a).unwrap();
+
+    img2.to_image_data();
+
+    unsafe { assert_eq!((*img.img).Xsize, (*img2.img).Xsize) };
+    unsafe { assert_eq!((*img.img).Ysize, (*img2.img).Ysize) };
+    unsafe { assert_eq!((*img.img).Bands, (*img2.img).Bands) };
+    unsafe { assert_eq!((*img.img).Length, (*img2.img).Length) };
+    unsafe { assert_eq!((*img.img).Compression, (*img2.img).Compression) };
+
+    println!("{:?}", img.img);
 }
--- src/optimal_image/compress/image.rs
+++ src/optimal_image/compress/image.rs
@@ -1,5 +1,10 @@
+extern crate image;
+extern crate imgref;
+extern crate rgb;
+use dataclients::ImageData;
 use std::error::Error;
 use std::ffi::{CStr, CString};
+use std::slice;
 use std::marker::PhantomData;
 use std::os::raw::{c_char, c_int, c_void};
 use std::ptr::null;
@@ -32,8 +37,18 @@ impl<'a> VipsImage<'a> {
         path: S,
     ) -> Result<VipsImage<'a>, Box<Error>> {
         let path = CString::new(path)?;
-        let c =
-            unsafe { vips::vips_image_new_from_file(path.as_ptr(), NULL_LIST) };
+        let c = unsafe {
+            let mut out: *mut *mut vips::VipsImage =
+                vips::vips_image_new() as *mut *mut vips::VipsImage;
+            let rgb_image =
+                vips::vips_image_new_from_file(path.as_ptr(), NULL_LIST);
+            // new_from_file can return RGB image, we have to add alpha in order
+            // to be sure to have an RGBA image
+            vips::vips_addalpha(rgb_image, out);
+            // as we are using a native VipsImage we need to manually deref it
+            vips::g_object_unref(rgb_image as *mut c_void);
+            *(out)
+        };
         result(c)
     }
 
@@ -54,6 +69,75 @@ impl<'a> VipsImage<'a> {
             0 => Ok(()),
             _ => Err(current_error().into()),
         }
+    }
+
+    pub fn from_buffer(
+        buf: *const c_void,
+        len: usize,
+    ) -> Result<VipsImage<'a>, Box<Error>> {
+        let c =
+            unsafe { vips::vips_image_new_from_buffer(buf, len, NULL_LIST) };
+        result(c)
+    }
+
+    pub fn from_memory(
+        data: *const c_void,
+        size: usize,
+        width: i32,
+        height: i32,
+        bands: i32,
+    ) -> Result<VipsImage<'a>, Box<Error>> {
+        let c = unsafe {
+            vips::vips_image_new_from_memory(
+                data,
+                size,
+                width,
+                height,
+                bands,
+                vips::VipsBandFormat::VIPS_FORMAT_UCHAR,
+            )
+        };
+        result(c)
+    }
+
+    pub fn from_image_data(
+        img: &ImageData,
+    ) -> Result<VipsImage<'a>, Box<Error>> {
+        let byte_data: Vec<u8> =
+            img.pixels().fold(Vec::new(), |mut data: Vec<u8>, pixel| {
+                let rgb::RGBA { r, g, b, a } = pixel;
+                data.push(r as u8);
+                data.push(g as u8);
+                data.push(b as u8);
+                data.push(a as u8);
+                data
+            });
+        let byte_data_ptr: *const c_void =
+            &byte_data as *const _ as *const c_void;
+        let bands: i32 = 4; // RGBA (4 bands)
+        let size = img.buf.len() * bands as usize;
+        let width = img.width() as i32;
+        let height = img.height() as i32;
+
+        let c =
+            VipsImage::from_memory(byte_data_ptr, size, width, height, bands)?;
+        result(c.img)
+    }
+
+    pub fn to_image_data(&self) -> Result<ImageData, Box<Error>> {
+        unimplemented!();
+        // let buffer: Vec<u8> = unsafe {
+        //     let len = (*self.img).length;
+        //     let array_ptr = vips::vips_image_write_to_memory(self.img, len as *mut usize);
+        //     let slice = slice::from_raw_parts(array_ptr, len);
+        //     slice.iter().map(|b| b).collect()
+        // };
+        // let width = unsafe { (*self.img).Xsize };
+        // let height = unsafe { (*self.img).Ysize };
+
+        // unsafe { println!("{:?}", buffer) };
+
+        // Ok(imgref::Img::new(buffer, width as usize, height as usize))
     }
 
     /// `jpegsave()` saves a `jpeg` image with a given `quality` factor
