43968136c26e0cf6ef4ae222ff293c3a8c174a35
Aditya Pratap Singh <adisinghrajput@gmail.com>
2018-08-30 09:01:14 +0200

    Remove unused variables

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,6 @@
 extern crate dssim;
 extern crate optimal_image;
 
-use optimal_image::compress::*;
-use optimal_image::dataclients::*;
-use optimal_image::encoders::*;
 use optimal_image::search::*;
 use optimal_image::ImageFormat;
 use std::env;
@@ -11,62 +8,16 @@ use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (_program, rest_args) = args.split_at(1);
-    let a = &rest_args[0];
-    let b = &rest_args[1];
-
-    let png_client = Png::new();
-    let jpg_client = Jpeg::new();
-    let mut context = dssim::new();
-    // let image_a = png_client.load(Path::new(a)).unwrap();
-    // let image_b = png_client.load(Path::new(b)).unwrap();
-    // let dssim_image_a = context.create_image(&image_a).unwrap();
-    // let dssim_image_b = context.create_image(&image_b).unwrap();
-
-    let image_a = jpg_client.load(Path::new(a)).unwrap();
-    let image_b = jpg_client.load(Path::new(b)).unwrap();
-    // let dssim_image_a = context.create_image(&image_a).unwrap();
-    // let dssim_image_b = context.create_image(&image_b).unwrap();
-
+    let image_path = &args.get(1).unwrap();
+    
     let mut search = Search::from_path(
-        Path::new(a),
+        Path::new(image_path),
         SearchOptions {
-            threshold: 0.0014,
+            threshold: 0.0005,
             quality_range: (70, 91),
             formats: vec![ImageFormat::JPEG],
         },
     ).unwrap();
     search.run();
     println!("{:?}", search.get_result());
-
-    // let (val, _) = context.compare(&dssim_image_a, dssim_image_b);
-
-    // println!("{:?}", val);
-
-    // let img = VipsImage::from_file(Path::new(a).to_str().unwrap()).unwrap();
-    // let img2 = VipsImage::from_file(Path::new(b).to_str().unwrap()).unwrap();
-    // let img2 = VipsImage::from_image_data(&image_a).unwrap();
-
-    // img2.to_image_data();
-
-    // unsafe { assert_eq!((*img.img).Xsize, (*img2.img).Xsize) };
-    // unsafe { assert_eq!((*img.img).Ysize, (*img2.img).Ysize) };
-    // unsafe { assert_eq!((*img.img).Bands, (*img2.img).Bands) };
-    // unsafe { assert_eq!((*img.img).length, (*img2.img).length) };
-    // unsafe { assert_eq!((*img.img).data, (*img2.img).data) };
-    // unsafe { println!("{:?} \n\n {:?}", (*img.img), (*img2.img)) };
-    // unsafe { println!("{:?} - {:?}", (*img.img).length, (*img2.img).length) };
-    // unsafe { assert_eq!((*img.img).Compression, (*img2.img).Compression) };
-
-    // println!("{:?}", img.img);
-
-    // let img = ImageJpegImage::from_image_data(&image_a).unwrap();
-    // let img2 = ImageJpegImage::from_image_data(&image_b).unwrap();
-    // let img3 = img.encode(85).unwrap();
-
-    // let dssim_image_a = context.create_image(&img.to_image_data().unwrap()).unwrap();
-    // let dssim_image_b = context.create_image(&img3.to_image_data().unwrap()).unwrap();
-    // let (val, _) = context.compare(&dssim_image_a, dssim_image_b);
-
-    // println!("{:?}", val);
 }
